9b42c41ed6c9efa56801b31ce8989dddbce70817
Lokesh Kumar <antilshivaay@gmail.com>
2026-05-25 07:07:06 +0530

    app.js

--- projects/weathernow/app.js
+++ projects/weathernow/app.js
@@ -1,5 +1,4 @@
-// WeatherNow - app.js
-// Get your free API key from https://openweathermap.org/api
+
 const API_KEY = '51b0f7baa81a0exxxxxxxxxxxx6';
 const BASE = 'https://api.openweathermap.org/data/2.5';
 
